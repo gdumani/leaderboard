0bb1940169ff009c8a884243208fdaf3c0e57707
gdumani <gdumani@gmail.com>
2022-01-28 00:56:06 -0600

    implement saving and retrieven form localstorage

--- src/getscores.js
+++ src/getscores.js
@@ -1,8 +1,10 @@
-const getScores = () => [
-  ['John', 50],
-  ['Peter', 100],
-  ['John', 75],
-  ['Peter', 120],
-];
+const getScores = localStorage.getItem('scores') !== null
+  ? JSON.parse(localStorage.getItem('scores'))
+  : [
+    ['John', 50],
+    ['Peter', 100],
+    ['John', 75],
+    ['Peter', 120],
+  ];
 
 export default getScores;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './style.css';
 import list from './list.js';
 import getScores from './getscores.js';
+import submitScore from './submitScore.js';
 
-let scores = getScores();
+let scores = getScores;
 list(scores);
 const refresh = document.getElementById('refresh');
 refresh.addEventListener('click', () => {
-  scores = getScores();
+  scores = getScores;
   list(scores);
 });
+const addScore = document.getElementById('add-score');
+addScore.addEventListener('click', () => submitScore());
